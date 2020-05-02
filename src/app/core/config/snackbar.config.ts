468387db0a3f826af8e0da7d74dde5da525962d0
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-05-02 16:57:29 +0530

    ✨ Update snackbar configuration

--- src/app/core/config/snackbar.config.ts
+++ src/app/core/config/snackbar.config.ts
@@ -4,7 +4,7 @@
  * File Created: Saturday, 2nd May 2020 4:41:55 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Saturday, 2nd May 2020 4:43:20 pm
+ * Last Modified: Saturday, 2nd May 2020 4:57:14 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
@@ -12,7 +12,7 @@
 import { MatSnackBarConfig } from '@angular/material/snack-bar';
 
 export const snackbarOptions: MatSnackBarConfig<any> = {
-  duration: 300000,
+  duration: 3000,
   panelClass: 'webitto-snackbar',
   verticalPosition: 'bottom',
   horizontalPosition: 'right',
